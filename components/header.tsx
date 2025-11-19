d0292259268248d89c1745d4a21da668697d6434
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-11-19 18:02:41 +0530

    update logo color

--- components/header.tsx
+++ components/header.tsx
@@ -17,7 +17,7 @@ export function Header() {
     <header className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="w-full flex h-16 items-center justify-between px-4">
         <div className="flex items-center gap-2">
-          <div className="flex h-9 w-9 items-center justify-center rounded-xl bg-gradient-to-br from-blue-500 to-cyan-500">
+          <div className="flex h-9 w-9 items-center justify-center rounded-xl bg-[#353536]">
             <Code2 className="h-5 w-5 text-white" />
           </div>
 
